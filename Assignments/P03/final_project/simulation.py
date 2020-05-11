7ede513b2d7610612114a0119f716a2a289b304b
duland94 <54681510+duland94@users.noreply.github.com>
2020-05-10 21:02:29 -0500

    Update simulation.py

--- Assignments/P03/final_project/simulation.py
+++ Assignments/P03/final_project/simulation.py
@@ -1,3 +1,22 @@
+#///////////////////////////////////////////////////////////////////////////////
+#//                   
+#// Author:           Dulan Dunusinghe
+#// Label:            P03
+#// Title:            COVID-19 Design
+#// Course:           CMPS 2143
+#// Semester:         Spring 2020
+#// 
+#// Description: 
+#//       
+#//       
+#//
+#// Usage:
+#//       
+#// Files: 
+#//
+#//        
+#/////////////////////////////////////////////////////////////////////////////////
+
 # Import and initialize the pygame library
 import pygame
 import random
